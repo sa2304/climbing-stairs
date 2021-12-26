184d4e2151a8d56d487480563a6ec4503262cf8d
Антон Шмидт <schmidt.ajax@gmail.com>
2021-12-26 18:08:12 +0700

    Solution: Dynamic programming
    Explanation https://www.youtube.com/watch?v=Y0lT9Fck7qI

--- main.cpp
+++ main.cpp
@@ -4,8 +4,14 @@
 class Solution {
  public:
   int climbStairs(int n) {
-    // FIXME
-    return n;
+    int one = 1, two = 1;
+    for (int i = n - 1; 0 < i; --i) {
+      int tmp = one;
+      one = one + two;
+      two = tmp;
+    }
+
+    return one;
   }
 };
 
@@ -15,6 +21,7 @@ void TestClimbStairs() {
   assert(2 == s.climbStairs(2));
   assert(3 == s.climbStairs(3));
   assert(5 == s.climbStairs(4));
+  assert(8 == s.climbStairs(5));
 }
 
 int main() {
